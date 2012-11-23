e92cc478b2d40fbbf4603bb8fa913bfc7eaf59ae
Richard Yao <ryao@gentoo.org>
2012-11-23 03:11:24 -0500

    Revert Fix NULL pointer deference in udev_hwdb_get_properties_list_entry
    Kay Sievers produced a better fix for this issue in systemd, so we
    revert this patch in favor of his.
    
    Signed-off-by: Richard Yao <ryao@gentoo.org>

--- src/udev/udev-builtin-hwdb.c
+++ src/udev/udev-builtin-hwdb.c
@@ -33,9 +33,6 @@ int udev_builtin_hwdb_lookup(struct udev_device *dev, const char *modalias, bool
         struct udev_list_entry *entry;
         int n = 0;
 
-        if (!hwdb)
-                return EXIT_FAILURE;
-
         udev_list_entry_foreach(entry, udev_hwdb_get_properties_list_entry(hwdb, modalias, 0)) {
                 if (udev_builtin_add_property(dev, test,
                                               udev_list_entry_get_name(entry),
